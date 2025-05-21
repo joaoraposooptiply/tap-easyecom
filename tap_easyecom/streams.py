e0134ac818eecbdc7b84fa2144cd4683fa21afc0
eduardosequeir4 <125479526+eduardosequeir4@users.noreply.github.com>
2025-05-21 20:08:17 +0100

    Merge pull request #29 from eduardosequeir4/main
    Type fix

--- tap_easyecom/streams.py
+++ tap_easyecom/streams.py
@@ -270,11 +270,10 @@ class SellOrdersStream(EasyEcomStream):
         th.Property("carrier_id", th.IntegerType),
         th.Property("awb_number", th.StringType),
         # TODO: what??
-        th.Property("Package Weight", th.CustomType({"type": ["string","number"]})),
-        th.Property("Package Height", th.CustomType({"type": ["string","number"]})),
-        th.Property("Package Length", th.CustomType({"type": ["string","number"]})),
-        th.Property("Package Width", th.CustomType({"type": ["string","number"]})),
-
+        th.Property("Package Weight", th.NumberType),
+        th.Property("Package Height", th.NumberType),
+        th.Property("Package Length", th.NumberType),
+        th.Property("Package Width", th.NumberType),
         th.Property("order_status", th.StringType),
         th.Property("order_status_id", th.IntegerType),
         th.Property("suborder_count", th.CustomType({"type": ["string","integer"]})),
@@ -317,7 +316,7 @@ class SellOrdersStream(EasyEcomStream):
         th.Property("billing_country", th.StringType),
         th.Property("billing_mobile", th.StringType),
         th.Property("order_quantity", th.IntegerType),
-        th.Property("meta", th.StringType),
+        th.Property("meta", th.ObjectType()),
         th.Property("documents", th.ObjectType(
             th.Property("originalLabelUrl", th.StringType),
             th.Property("easyecom_invoice", th.StringType),
@@ -459,7 +458,7 @@ class ReturnsStream(EasyEcomStream):
         th.Property("credit_note_number", th.StringType),
         th.Property("marketplace_credit_note_num", th.StringType),
         th.Property("marketplace_invoice_num", th.StringType),
-        th.Property("batch_id", th.StringType),
+        th.Property("batch_id", th.NumberType),
         th.Property("batch_created_at", th.DateTimeType),
         th.Property("payment_mode", th.StringType),
         th.Property("payment_mode_id", th.IntegerType),
