cc53f925c25880ecb50a7f75106ae4aad0b61098
Vinicius Ghellere Sbardelotto <viniciusghellere@gmail.com>
2025-05-22 19:05:08 -0300

    fix(HGI-7942): fix field types for `SellOrdersStream` and `ReturnsStream` (#31)

--- tap_easyecom/streams.py
+++ tap_easyecom/streams.py
@@ -274,6 +274,7 @@ class SellOrdersStream(EasyEcomStream):
         th.Property("Package Height", th.NumberType),
         th.Property("Package Length", th.NumberType),
         th.Property("Package Width", th.NumberType),
+
         th.Property("order_status", th.StringType),
         th.Property("order_status_id", th.IntegerType),
         th.Property("suborder_count", th.CustomType({"type": ["string","integer"]})),
@@ -458,7 +459,7 @@ class ReturnsStream(EasyEcomStream):
         th.Property("credit_note_number", th.StringType),
         th.Property("marketplace_credit_note_num", th.StringType),
         th.Property("marketplace_invoice_num", th.StringType),
-        th.Property("batch_id", th.NumberType),
+        th.Property("batch_id", th.IntegerType),
         th.Property("batch_created_at", th.DateTimeType),
         th.Property("payment_mode", th.StringType),
         th.Property("payment_mode_id", th.IntegerType),
@@ -483,6 +484,6 @@ class ReturnsStream(EasyEcomStream):
         th.Property("order_quantity", th.IntegerType),
         th.Property("total_invoice_amount", th.NumberType),
         th.Property("total_invoice_tax", th.NumberType),
-        th.Property("invoice_collectable_amount", th.IntegerType),
+        th.Property("invoice_collectable_amount", th.NumberType),
         th.Property("items", th.CustomType({"type": ["array", "string"]})),
     ).to_dict()
